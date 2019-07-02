61f7f917d3064132cb88fcce23e268efe2e20856
chMatvey <matvey.chudakov@arcadia.spb.ru>
2019-07-02 11:40:10 +0300

    react

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {connect} from "react-redux";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>HW</h1>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    null,
+    null
+)(App)
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux'
+
+const store = createStore((state = 0, action) => {
+    return state
+})
+
+export default store
